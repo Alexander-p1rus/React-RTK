c9299f881589f4dd50e43e7a7c9e58be45d298a9
alexander <alexanderulko20000@gmail.com>
2023-02-22 18:55:41 +0500

    learned default RTK

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useSelector} from "react-redux";
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
+import {userSlice} from "./store/reducers/UserSlice";
+import {fetchUsers} from "./store/reducers/ActionCreators";
 
 const App = () => {
+    const dispatch = useAppDispatch()
+    const {users,error,isLoading}=useAppSelector(state => state.userReducer)
+    useEffect(()=>{
+        dispatch(fetchUsers())
+    },[])
+
+    if(error){
+        return <div>ERROR</div>
+    }
+
+    if(isLoading){
+        return <div>LOADING...</div>
+    }
+
     return (
         <div>
-          
+            {JSON.stringify(users)}
         </div>
     );
 };
